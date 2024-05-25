e99acfa323a2fe0a9b0b7f953f5c6c7b2ef600de
AkaleshTanwer <akaleshtanwer5014@gmail.com>
2024-05-25 20:00:02 +0530

    created pages

--- src/pages/Movies/MovieList.jsx
+++ src/pages/Movies/MovieList.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import MovieCard from './MovieCard';
 import { fetchInitialMovies, fetchMovies } from '../../redux/actions/movieActions';
+import MovieCard from '../../components/MovieCard';
 
 const MovieList = () => {
   const [query, setQuery] = useState('');
