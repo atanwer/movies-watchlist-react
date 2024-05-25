d101f1e1fd6370c80e21bfe9a1706c7b21f3434a
AkaleshTanwer <akaleshtanwer5014@gmail.com>
2024-05-25 19:16:59 +0530

    added pages folder

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import { useSelector } from 'react-redux';
-import MovieList from './components/Movies/MovieList';
-import Login from './components/Auth/Login';
-import SignUp from './components/Auth/SignUp';
+import MovieList from './pages/Movies/MovieList';
+import Login from './pages/Auth/Login';
+import SignUp from './pages/Auth/SignUp';
 import ProtectedRoute from './utills/ProtectedRoute';
 import Header from './components/Header';
 
