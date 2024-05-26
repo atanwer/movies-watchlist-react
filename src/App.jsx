d4b4485a8483773b0690af831cf7148854507773
AkaleshTanwer <akaleshtanwer5014@gmail.com>
2024-05-26 13:24:20 +0530

    created movie details page

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import SignUp from './pages/Auth/SignUp';
 import ProtectedRoute from './utills/ProtectedRoute';
 import Header from './components/Header';
 import Watchlist from './pages/Movies/Watchlist';
+import MovieDetails from './pages/Movies/MovieDetails';
 
 function App() {
   const isAuthenticated = useSelector(state => state.auth.isAuthenticated);
@@ -16,6 +17,7 @@ function App() {
       <Header/>
       <Routes>
         <Route path="/" element={<ProtectedRoute>{isAuthenticated ? <MovieList /> : <Navigate to="/login" replace />}</ProtectedRoute>} />
+        <Route path="/movie/:id" element={<ProtectedRoute>{isAuthenticated ? <MovieDetails /> : <Navigate to="/login" replace />}</ProtectedRoute>} />
         <Route path="/watchlist" element={<ProtectedRoute>{isAuthenticated ? <Watchlist /> : <Navigate to="/login" replace />}</ProtectedRoute>} />
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<SignUp />} />
--- src/pages/Movies/MovieDetails.jsx
+++ src/pages/Movies/MovieDetails.jsx
@@ -1,9 +1,43 @@
-import React from 'react'
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { useParams } from 'react-router-dom';
+import { fetchMovieDetail } from '../../redux/actions/movieActions';
 
 const MovieDetails = () => {
-  return (
-    <div>MovieDetails</div>
-  )
-}
+  const { id } = useParams();
+  const dispatch = useDispatch();
+  const movie = useSelector(state => state.movies.movieDetail);
+  const loading = useSelector(state => state.movies.loading);
+  const error = useSelector(state => state.movies.error);
 
-export default MovieDetails
+  useEffect(() => {
+    dispatch(fetchMovieDetail(id));
+  }, [dispatch, id]);
+
+  return (
+    <div className="movie-details-container">
+      {loading ? (
+        <p className="loading-message">Loading...</p>
+      ) : error ? (
+        <p className="error-message">Error: {error}</p>
+      ) : movie ? (
+        <div className="movie-details">
+          <img src={movie.Poster} alt={movie.Title} className="movie-details-poster" />
+          <div className="movie-details-info">
+            <h2 className="movie-details-title">{movie.Title}</h2>
+            <p className="movie-details-plot">{movie.Plot}</p>
+            <p><strong>Genre:</strong> {movie.Genre}</p>
+            <p><strong>Released:</strong> {movie.Released}</p>
+            <p><strong>Director:</strong> {movie.Director}</p>
+            <p><strong>Actors:</strong> {movie.Actors}</p>
+            <p><strong>IMDB Rating:</strong> {movie.imdbRating}</p>
+          </div>
+        </div>
+      ) : (
+        <p className="no-movie-message">Movie not found</p>
+      )}
+    </div>
+  );
+};
+
+export default MovieDetails;
