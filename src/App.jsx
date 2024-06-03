98cfbb25234affceb31d58747a37fcbd269ded15
AkaleshTanwer <akaleshtanwer5014@gmail.com>
2024-06-03 11:16:27 +0530

    code optimized

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
-import { useSelector } from 'react-redux';
 import MovieList from './pages/Movies/MovieList';
 import Login from './pages/Auth/Login';
 import SignUp from './pages/Auth/SignUp';
@@ -10,15 +9,14 @@ import Watchlist from './pages/Movies/Watchlist';
 import MovieDetails from './pages/Movies/MovieDetails';
 
 function App() {
-  const isAuthenticated = useSelector(state => state.auth.isAuthenticated);
 
   return (
     <Router>
       <Header/>
       <Routes>
-        <Route path="/" element={<ProtectedRoute>{isAuthenticated ? <MovieList /> : <Navigate to="/login" replace />}</ProtectedRoute>} />
-        <Route path="/movie/:id" element={<ProtectedRoute>{isAuthenticated ? <MovieDetails /> : <Navigate to="/login" replace />}</ProtectedRoute>} />
-        <Route path="/watchlist" element={<ProtectedRoute>{isAuthenticated ? <Watchlist /> : <Navigate to="/login" replace />}</ProtectedRoute>} />
+        <Route path="/" element={<ProtectedRoute>{<MovieList /> }</ProtectedRoute>} />
+        <Route path="/movie/:id" element={<ProtectedRoute>{<MovieDetails /> }</ProtectedRoute>} />
+        <Route path="/watchlist" element={<ProtectedRoute>{<Watchlist /> }</ProtectedRoute>} />
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<SignUp />} />
       </Routes>
